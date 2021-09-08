76736186833c5dddf5fe01a8b4dc145943cb1404
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2021-09-09 01:56:22 +0300

    Lesson 2: Homework

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+
+import bg1 from './assets/bg1.jpg';
+import bg2 from './assets/bg2.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React.JS!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header title={'This is title'} descr={'This is Description!'} />
+            <Layout title={'Layout 1'} descr={'Description of Layout 1'} urlBg={bg1} />
+            <Layout title={'Layout 2'} descr={'Description of Layout 2'} colorBg={'red'} />
+            <Layout title={'Layout 3'} descr={'Description of Layout 3'} urlBg={bg2} />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
